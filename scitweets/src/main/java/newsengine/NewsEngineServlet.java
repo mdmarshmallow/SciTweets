58622981e1fe917f0d8edce253c305fc9f325bb4
mdmarshmallow <marcd2000@gmail.com>
2017-10-28 14:07:09 -0700

    Added comments

--- scitweets/src/main/java/newsengine/NewsEngineServlet.java
+++ scitweets/src/main/java/newsengine/NewsEngineServlet.java
@@ -21,6 +21,7 @@ import twitter4j.TwitterException;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/newsengine")
+//serves the newsengine.jsp
 public class NewsEngineServlet extends HttpServlet {
 
 	private TweetService tweetService = new TweetService();
@@ -28,19 +29,25 @@ public class NewsEngineServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//sets sends the categories list and the method to the jsp
 		request.setAttribute("categories", categoryService.retrieveCategories());
 		request.setAttribute("method", "get");
 		request.getRequestDispatcher("/WEB-INF/views/newsengine.jsp").forward(request, response);
 	}
 
+	//doPost is called when a handle is clicked on the page
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
+			//gets a list of the tweets associated with that user (they are filtered in the retrieveTweets function)
 			List<STweet> tweetList = tweetService.retrieveTweets(request.getParameter("user"));
+			//adds the tweets and the number of tweets 
 			request.setAttribute("tweets", tweetList);
 			request.setAttribute("numOfArticles", (tweetList).size());
+			//gets the month and year of the oldest tweet on the list
 			int month = tweetList.get(tweetList.size() - 1).getMonth();
 			int year = tweetList.get(tweetList.size() - 1).getYear();
+			//calculate months calculates the number of months from now and the first tweet
 			int difference = calculateMonths(month, year);
 			if (difference != 0){
 				request.setAttribute("months", difference);
@@ -49,20 +56,24 @@ public class NewsEngineServlet extends HttpServlet {
 			}
 		} catch (SQLException | TextAPIException | TwitterException | InstantiationException | IllegalAccessException
 				| ClassNotFoundException e) {
+			//no error handling again :(
 			e.printStackTrace();
 		}
+		//resends the categories and the sends the name of the tweet's author back
 		request.setAttribute("name", request.getParameter("name"));
 		request.setAttribute("categories", categoryService.retrieveCategories());
 		request.setAttribute("method", "post");
 		request.getRequestDispatcher("/WEB-INF/views/newsengine.jsp").forward(request, response);
 	}
 
+	//function calculates the difference between the current month and the inputted month given the year
 	private int calculateMonths(int month, int year) {
 		Date date = new Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
 		int currentMonth = calendar.get(Calendar.MONTH);
 		int currentYear = calendar.get(Calendar.YEAR);
+		//this is where the calculation happens
 		int difference = (((currentYear - year) * 12) + currentMonth) - month;
 		return difference;
 	}
