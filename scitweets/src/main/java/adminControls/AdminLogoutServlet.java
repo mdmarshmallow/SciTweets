57be5af1446b6da2b63f43e4faf1ab6165ab51b9
mdmarshmallow <marcd2000@gmail.com>
2017-10-31 10:58:07 -0700

    Added comments

--- scitweets/src/main/java/adminControls/AdminLogoutServlet.java
+++ scitweets/src/main/java/adminControls/AdminLogoutServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns="/adminlogout")
+//removes the username from the session and redirects the user to the admin login page
 public class AdminLogoutServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		request.getSession().removeAttribute("username");
--- scitweets/src/main/java/adminControls/RemoveCategory.java
+++ scitweets/src/main/java/adminControls/RemoveCategory.java
@@ -15,6 +15,7 @@ import dbconnection.DBConnect;
 @WebServlet(urlPatterns = "/removecategory")
 public class RemoveCategory extends HttpServlet {
 	
+	//doesn't do anything if the request is a GET request except to forward the user to the admin control servlet (filter is still applied)
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		request.getRequestDispatcher("/admincontrol").forward(request, response);
 	}
@@ -22,6 +23,7 @@ public class RemoveCategory extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
+			//uses the DBConnect class to remove the category from the database, then redirects to the admincontrol JSP
 			DBConnect.deleteCategory(request.getParameter("category"));
 			request.getRequestDispatcher("/admincontrol").forward(request, response);
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
